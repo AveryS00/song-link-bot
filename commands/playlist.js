f473ef817f203069a6768b9dbffc2b6c1975db8d
AveryS00 <ajsmith2@wpi.edu>
2020-10-18 13:05:45 -0400

    merge

--- commands/playlist.js
+++ commands/playlist.js
@@ -6,7 +6,7 @@ module.exports = {
 	description: 'Gives a link to the playlist of collected songs',
 	execute(message, args, json, spotify) {
 		message.channel.send(`View playlist here: https://open.spotify.com/user/${spotify.getSpotifyUserId()}/playlist/${json.server_list[message.guild.id].playlist_id}`);
-	console.log(`${message.member.displayName} requested viewing of link in guild ${message.guild.id}`);
+	console.log(`${message.member.user.tag} requested viewing of link in guild ${message.guild.id}`);
 	}
 };
 
